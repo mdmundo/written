4f933f32c0ea7b38175392211c991c2c81acec00
Edmundo Paulino <manyymoore@gmail.com>
2021-11-05 19:30:51 +0000

    chore: 💩 Needs improvement

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 #[cfg(test)]
 mod tests {
     #[test]
+    fn correct_approach() {
+        let numbers = "654312".chars().collect::<Vec<char>>();
+        let chunks = numbers.rchunks(3).enumerate();
+
+        for number in chunks {
+            match number {
+                (pow, values) => println!("{} {:#?}", pow, values),
+            }
+        }
+    }
+
+    #[test]
     fn input_approach() {
         let input = "123";
         let mut number = input.chars().enumerate();
@@ -24,7 +36,7 @@ mod tests {
     }
 
     #[test]
-    fn approach() {
+    fn gen_approach() {
         fn unit(val: char) -> Option<&'static str> {
             match val {
                 '1' => Some("Um"),
@@ -38,5 +50,34 @@ mod tests {
             }
         }
         assert_eq!(gen(0, '1'), Some("Um"));
+    }
+
+    #[test]
+    fn approach() {
+        fn unit(val: char) -> Option<&'static str> {
+            match val {
+                '1' => Some("Um"),
+                _ => None,
+            }
+        }
+        fn gen(pow: usize, val: char) -> Option<&'static str> {
+            match pow {
+                0 => unit(val),
+                _ => None,
+            }
+        }
+        let number = "1".chars().enumerate();
+
+        let mut result = String::new();
+        for unit in number {
+            if !result.is_empty() {
+                result.push_str(" e ");
+            }
+            result.push_str(match unit {
+                (pow, val) => gen(pow, val).expect("Invalid"),
+            })
+            // this approach is wrong.
+            // how 19 would be?
+        }
     }
 }
